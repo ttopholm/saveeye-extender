6760ad0e5f2133cfdd69afc48ba8439cb04d2170
Tue Topholm <tt@gaijin.dk>
2024-12-15 20:25:15 +0000

    small fix

--- mqtt.py
+++ mqtt.py
@@ -1,28 +1,29 @@
-from paho.mqtt import client as mqtt_client
-import random
-import time
+import paho.mqtt.client as mqtt
 import json
-import math
-import statistics
-import os
+from environs import Env
+
+env = Env()
+env.read_env()
+
+# variables required
+mqtt_broker = env.str('mqtt-broker')
+
+# Optional
+mqtt_port = env.int('mqtt-port', 1883)
+mqtt_username = env.str('mqtt-username', None)
+mqtt_password = env.str('mqtt-password', None)
+subscribe_topic = env.str('subscribe-topic', 'saveeye/telemetry')
+publish_topic = env.str('publish-topic', 'saveeye/telemetry_calculated')
 
 
-mqtt_host = os.environ.get('mqtt-host', '127.0.0.1')
-port = os.environ.get('mqtt-port', 1883)
-mqtt_username = os.environ.get('mqtt-username', None)
-mqtt_password = os.environ.get('mqtt-password', None)
-subscribe_topic = os.environ.get('subscribe-topic', 'saveeye/telemetry')
-publish_topic = os.environ.get('publish-topic', 'saveeye/telemetry_calculated')
-
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
 last_record = {}
 last_values = []
 
 def connect_mqtt():
-    client = mqtt_client.Client(client_id)
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     if mqtt_username or mqtt_password:
         client.username_pw_set(username=mqtt_username, password=mqtt_password)
-    client.connect(mqtt_host, port)
+    client.connect(mqtt_host, mqtt_port)
     return client
 
 
